155e79ec41031d06baa0cb6f7e665020ae3d4b52
Aaryan <aaryank0810@gmail.com>
2026-08-05 12:45:20 +0530

    Book Constructor

---
+++ script.js
@@ -0,0 +1,26 @@
+const library = [];
+
+function Book(title , author , pages , read)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function()
+    {
+       return `{this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary()
+{
+    const createBook = new Book(title.value , author.value , pages.value , read.checked);
+    library.push(createBook);
+}
+
+function displayBooks()
+{
+    library.forEach((book) => {
+        console.log(book.info());
+    })
+}
