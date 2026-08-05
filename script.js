330124da570801aba47359f90a7397a3bc90f081
Aaryan <aaryank0810@gmail.com>
2026-08-05 14:58:56 +0530

    Basic JS logic added

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const library = [];
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
 const dialog = document.getElementById('formPopup');
 const openPopup = document.getElementById('addBook');
+const closePopup = document.getElementById('close-btn');
+const submit = document.getElementById('submit');
 
 openPopup.addEventListener('click' , () => {
     dialog.showModal();
-})
+});
+
+closePopup.addEventListener('click' , () => {
+    dialog.close();
+});
+
+submit.addEventListener('click' , (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    resetForm();
+});
+
+//Book Constructor
 function Book(title , author , pages , read)
 {
     this.title = title;
@@ -13,19 +31,37 @@ function Book(title , author , pages , read)
     this.read = read;
     this.info = function()
     {
-       return `{this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+       return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read ? "read" : "not read yet"
+}`;
     }
 }
 
+//Add book to Library
 function addBookToLibrary()
 {
-    const createBook = new Book(title.value , author.value , pages.value , read.checked);
+    const createBook = new Book(
+        title.value , author.value , pages.value , read.checked
+    );
     library.push(createBook);
+    console.log('Book added to library');
+    displayBooks();
 }
+
+//Display books
 
 function displayBooks()
 {
     library.forEach((book) => {
         console.log(book.info());
     })
+}
+
+//Reset the form
+function resetForm()
+{
+    this.title = '';
+    this.author = '';
+    this.pages = '';
+    this.read = false;
 }
